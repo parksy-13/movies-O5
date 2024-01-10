245862d01af13fafb3601308912bb2621356ac49
parksy-13 <psy9905@gmail.com>
2024-01-10 21:02:56 +0900

    refactor:영화 수 20개로 증가

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const service= config.apikey;
 
-const url1= `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${service}`
+const url1= `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${service}&itemPerPage=20`
 const url2 = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${service}`
 
 fetch(url1)
