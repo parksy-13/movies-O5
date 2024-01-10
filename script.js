cb88e084f391fabcaff468c21528a5de2ac899ef
parksy-13 <psy9905@gmail.com>
2024-01-10 16:25:17 +0900

    css,js,api 수정

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const service= config.apikey;
+
+const url = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${service}`
+
+fetch(url)
+  .then(response => response.json())
+  .then(response => console.log(response))
