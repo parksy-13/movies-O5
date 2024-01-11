c4bd5246034f93618a826b31aeb41457a3ef8894
parksy-13 <psy9905@gmail.com>
2024-01-11 20:35:57 +0900

    docs: 주석, 코드 정리

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const service = config.apikey;
 
 const urlBoxoffice = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=${service}&targetDt=20231229` //주간/주말 박스오피스
-// const urlInformation = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${service}` //영화 상세 정보
-// const urlList = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${service}&itemPerPage=20` //영화 목록
+const urlInformation = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${service}` //영화 상세 정보
+const urlList = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${service}&itemPerPage=20` //영화 목록
 
 // 박스오피스 정보로 영화 카드 생성
 
@@ -48,11 +48,9 @@ if(movieNameInput===""){
     cardNameArr[i] = cardArr[i].getElementsByTagName('h2')[0].innerText;
     cardNameArr[i].indexOf(movieNameInput)>-1? cardArr[i].style = 'display:inline-block': cardArr[i].style = 'display:none';
        }
-  if(cardArr.style ==='display:none'){
-    alert("검색 결과가 없습니다.");
-  }
 }
 }
+
 // 영화 상세 정보(배우명, 역할명) 추가 -> 다른 html,js에 필요한 정보입니다! 이 fetch함수 복붙하시고 두번째 then에 이어서 실행해주세요!
 
 // fetch(urlInformation)
